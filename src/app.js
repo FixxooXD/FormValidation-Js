b6ec4105f48fae4e4200b6cc58eef7a875751ab1
Fixxoo <rajpalmal40@gmail.com>
2022-12-05 12:45:59 +0530

    v1

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const inputName = document.querySelector('input[type="text"]');
+const inputEmail = document.querySelector('input[type="email"]');
+const inputpassword = document.querySelector('input[type="password"]');
+const confirmPassword = document.getElementById('confirmPassword')
+const submitBtn = document.getElementById('submitBtn');
+
+
+submitBtn.addEventListener('click',(e)=>{
+  if(inputpassword.value == confirmPassword.value){
+    alert('good to go');
+  }
+  else{
+    alert('Password and Correct Password Dont match');
+    e.preventDefault();
+  }
+})
